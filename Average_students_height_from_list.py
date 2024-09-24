207557c63dafdebd46c33d9316198320a742831a
Azegba Kizito Amandianeze <kizitoamandi96@gmail.com>
2024-09-24 11:53:50 +0100

    Added a program that finds the highest number in a lsit

--- Average_students_height_from_list.py
+++ Average_students_height_from_list.py
@@ -9,17 +9,19 @@ for n in range(0, len(student_heights)):
 
 print(student_heights)
 
-
+#Getting the sum of the total heights in a list
 total_heights = 0
 for height in student_heights:
     total_heights += height
 print(total_heights)
 
+#Getting the number os heights in a list
 student_number = 0
 for student_num in student_heights:
     student_number += 1
 print(student_number)
 
+#calculating the average height using the formular below and then round function
 
 Average_height = round(total_heights / student_number)
 print(Average_height)
